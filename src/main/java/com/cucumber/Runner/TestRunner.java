7ff4cd98d54a10fd824017b0af8f064f8ca39a4e
Abhishek Singh <Abhishek.topa@gmail.com>
2019-01-12 12:45:21 +0530

    Implementing Extent Report

--- src/main/java/com/cucumber/Runner/TestRunner.java
+++ src/main/java/com/cucumber/Runner/TestRunner.java
@@ -1,6 +1,9 @@
 package com.cucumber.Runner;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -8,10 +11,15 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"C:\\Users\\ABHISHEK\\eclipse-workspace\\CucumberDemo\\src\\main\\java\\com\\cucumber\\FeatureFiles\\LICLogin.feature"},
-		glue = {"stepDefination"},
+		glue = {"stepDefination"},plugin = {
+		"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html" },
 		dryRun = false,
 		tags= {"@LICLogin,@ValidatesPolicies,@LICLogOut"}
 )
 public class TestRunner {
+	@AfterClass
+	public static void writeExtentReport() {
+		Reporter.loadXMLConfig("C:\\Users\\ABHISHEK\\BDDCucumber-Workspace\\BDDCucumber\\src\\main\\java\\com\\properties\\extent-config.xml");
+	}
 
 }
